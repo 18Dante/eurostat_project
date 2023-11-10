15c5d28ae741f5c018d7c03755b82075b067bcad
michal_wrotniak <139686742+MichalWrotniak@users.noreply.github.com>
2023-11-10 12:23:47 +0100

    Script generating regional structure

--- Region hierarchy.sql
+++ Region hierarchy.sql
@@ -13,13 +13,13 @@ SELECT
 	NULL AS parent,
 	'NUTS_3_METRO' AS child,
 	'NUTS 3 Metropolitan Regions' AS label,
-	0 AS level
+	1 AS level
 UNION
 SELECT 
 	IIF(LEN(mr.region_code) = 2, 'NUTS_3_METRO', LEFT(mr.region_code, 2)) AS parent,
 	mr.region_code AS child,
 	mr.label,
-	IIF(LEN(mr.region_code) = 2, 1, 2) AS level
+	IIF(LEN(mr.region_code) = 2, 2, 3) AS level
 FROM eurostat_project.metropolitan_regions AS mr -- You need to align the schema and table name with your database
 ),
 
@@ -31,7 +31,7 @@ n0.parent,
 n0.child,
 n0.level,
 n0.label,
-CAST('NUTS_3_METRO' AS nvarchar(50)) AS path
+CAST('NUTS_3_METRO' AS nvarchar(MAX)) AS path
 FROM nuts_metro_regions AS n0
 WHERE n0.parent IS NULL
 
@@ -42,7 +42,7 @@ n.parent,
 n.child,
 n.level,
 n.label,
-CAST(CONCAT_WS('|', ns.path, n.child) AS nvarchar(50)) AS path
+CAST(CONCAT_WS('|', ns.path, n.label) AS nvarchar(MAX)) AS path
 FROM nuts_metro_regions AS n
 INNER JOIN nuts_structure AS ns ON ns.child = n.parent
 
